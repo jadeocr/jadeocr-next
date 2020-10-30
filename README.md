a626a92ab021468187a079916d66c6d46ff1367c
TanayB11 <tanaybiradar24@gmail.com>
2020-10-30 16:48:07 -0700

    Add precommit formatting hook

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # jadeocr-next
-
-# TODO
--- client/src/components/AuthForm.vue
+++ client/src/components/AuthForm.vue
@@ -80,6 +80,12 @@
         confirmPassword: '',
       }
     },
+    methods: {
+      checkValidEmail(email: string): boolean {
+        const re = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,4}))$/
+        return re.test(email)
+      },
+    },
   })
 </script>
 
