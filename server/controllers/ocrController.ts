c73e9ec79acb0c1734aca3d8bb804e8dc363250c
wizard1238 <49770335+wizard1238@users.noreply.github.com>
2020-10-30 20:18:47 -0700

    Added auth

--- server/controllers/ocrController.ts
+++ server/controllers/ocrController.ts
@@ -1,14 +1,21 @@
 var hanziLookup = require('../public/hanzilookup/hanzilookup.min.js')
 var mmah = require('../public/hanzilookup/mmah.json')
 
-exports.get = function(req, res, next) {
+exports.post = function(req, res, next) {
   hanziLookup.init('mmah', JSON.stringify(mmah) , fileLoaded)
 
   function fileLoaded(arg) {
     if (!arg) {console.log('ocr database loading failed')}
   }
 
-  var lineArray = JSON.parse(Object.keys(req.body)[0])
+  var lineArray = []
+  if (JSON.parse(Object.keys(req.body)[0])) {
+    lineArray = JSON.parse(Object.keys(req.body)[0])
+  } else {
+    lineArray = [[[75,128],[83,172],[85,196]],[[84,127],[91,124],[123,121],[151,121],[161,121],[161,128],[165,157],[164,181],[162,195]],[[89,187],[103,194],[129,200],[149,199]]]
+    // gives character 口
+  }
+
 
   function lookup(strokeArray) {
     try {
