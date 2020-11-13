d0e4b209854085aec89f377549c938d837a070fe
Tanay Biradar <tanaybiradar24@gmail.com>
2020-11-12 19:36:38 -0800

    Add sidebar (#4)

--- client/src/shims-vue.d.ts
+++ client/src/shims-vue.d.ts
@@ -3,4 +3,5 @@ declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent
   export default component
+  declare module 'jquery'
 }
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    Welcome to the Dashboard
+  <div class="dashboard">
+    <sidebar/>
   </div>
 </template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import Sidebar from '../components/Sidebar.vue'
+
+  export default defineComponent({
+    name: 'Dashboard',
+    components: {
+      Sidebar,
+    },
+  })
+</script>
