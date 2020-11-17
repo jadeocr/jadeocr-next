adcbd8c22efcc1b1061a8fa37f385ace4132c4f6
Tanay Biradar <tanaybiradar24@gmail.com>
2020-11-16 20:40:58 -0800

    Add Sidebar (#6)
    * Add sidebar
    
    * Configure sidebar
    
    * Optimize for screen scales and lint

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="dashboard">
-    <sidebar/>
+    <div class="grid grid-cols-1 md:grid-cols-4">
+      <div class="col-span-1">
+        <sidebar />
+      </div>
+      <div class="mt-12 md:mt-16 col-span-1 page-content">
+        <div
+          class="mx-6 text-2xl font-normal md:mx-4 md:text-3xl lg:mx-10 xl:mx-20"
+        >
+          Dashboard
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
