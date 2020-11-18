837fae37971e1ac471be389041c21e8d25f680bc
wizard1238 <49770335+wizard1238@users.noreply.github.com>
2020-11-18 14:05:27 -0800

    Remodel users (#8)
    * added create class, join class, leave class, assign, unassign
    
    * signin gives user data, added a few more class features
    
    * setup joining class endpoint, fixed bugs
    
    * added validation for deck creation
    
    * added functionaliy to remove class
    
    * added assign deck function
    
    * added first and last name to user models
    
    * added first and last name to deck creation
    
    * added getting assigned decks
    
    Co-authored-by: Tanay Biradar <tanaybiradar24@gmail.com>

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,33 +1,69 @@
 var userModel = require('../models/userModel')
+var userDetailedModel = require('../models/userDetailedModel')
 var bcrypt = require('bcryptjs')
+var { validationResult } = require('express-validator')
 
 exports.signup = function(req, res, next) {
-  let email = req.body.email
-  let password = req.body.password
-  let confirmPassword = req.body.confirmPassword
-  let isTeacher = false
+  let errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    res.send(errors)
+  } else {
+    let email = req.body.email
+    let firstName = req.body.firstName
+    let lastName = req.body.lastName
+    let password = req.body.password
+    let confirmPassword = req.body.confirmPassword
+    let isTeacher = false
 
-  userModel.findOne({ email: email }, function(err, found) {
-    if (found) {
-      res.sendStatus(400)
-    } else if (!(email && password && confirmPassword)) {
-      res.sendStatus(400)
-    } else if (password != confirmPassword) {
-      res.sendStatus(400)
-    } else {
-      bcrypt.hash(password, 10, function(err, hash) {
-        if (err) console.log(err)
-        var newUser = new userModel({
-          email: email,
-          password: hash,
-          isTeacher: isTeacher,
-        })
-        newUser.save(function(err) {
+    userModel.findOne({ email: email }, function(err, found) {
+      if (found) {
+        res.status(400).send('Email already in use')
+      } else if (password != confirmPassword) {
+        res.status(400).send('Passwords do not match')
+      } else {
+        bcrypt.hash(password, 10, function(err, hash) {
           if (err) console.log(err)
-          res.sendStatus(200)
+          var newUser = new userModel({
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: hash,
+            isTeacher: isTeacher,
+          })
+          newUser.save(function(err) {
+            if (err) console.log(err)
+            userModel.findOne({email: email}, function(err, user) {
+              let detailedUser = new userDetailedModel({
+                id: String(user._id),
+                email: user.email,
+                firstName: firstName,
+                lastName: lastName,
+                password: user.password,
+                isTeacher: user.isTeacher,
+              })
+              detailedUser.save(function(err) {
+                if (err) console.log(err)
+              res.sendStatus(200)
+              })
+            })
+          })
         })
-      })
+      }
+    })
+  }
+}
+
+exports.user = function(req, res, next) {
+  res.send(req.user)
+}
+
+exports.details = function(req, res, next) {
+  userDetailedModel.findOne({id: req.user._id}, function(err, user) {
+    if (err) {
+      console.log(err)
+      res.send('There was an error')
+    } else {
+      res.send(user)
     }
   })
 }
-
--- server/models/userDetailedModel.ts
+++ server/models/userDetailedModel.ts
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose')
 
 var userDetailedSchema = new mongoose.Schema({
+    id: String,
     email: String,
+    firstName: String,
+    lastName: String,
     password: String,
     isTeacher: Boolean,
     classes: Array,
