e512ab66df3e4c427a73b1408a8189adffe91e97
wizard1238 <49770335+wizard1238@users.noreply.github.com>
2020-11-18 20:30:22 -0800

    removed cors extra stuffs

--- server/app.ts
+++ server/app.ts
@@ -66,21 +66,9 @@ app.use(cookieSession({
 app.use(passport.initialize())
 app.use(passport.session())
 
-let whitelist = ['http://localhost:8080']
-
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin 
-    if (!origin) return callback(null, true);
-    if (whitelist.indexOf(origin) === -1) {
-      var message = 'The CORS policy for this origin doesn\'t ' +
-      'allow access from the particular origin.';
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  },
+  origin: 'http://localhost:8080',
   credentials: true,
-  exposedHeaders: ["set-cookie"],
 }))
 
 // view engine setup
