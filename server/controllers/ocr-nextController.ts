4949936134943cd14b9438b467a8059214594c6a
wizard1238 <49770335+wizard1238@users.noreply.github.com>
2020-11-20 16:21:20 -0800

    Update ocr-nextController.ts

--- server/controllers/ocr-nextController.ts
+++ server/controllers/ocr-nextController.ts
@@ -3,7 +3,7 @@ const spawn= require('child_process').spawn
 exports.ocr = function(req, res, next) {
     var strokes = req.body.strokes || "[[(75,128),(83,172),(85,196)],[(84,127),(91,124),(123,121),(151,121),(161,121),(161,128),(165,157),(164,181),(162,195)],[(89,187),(103,194),(129,200),(149,199)]]"
 
-    var ocrProcess = spawn('python', [__dirname + '\\ocr.py',
+    var ocrProcess = spawn('python', [__dirname + '/ocr.py',
                          strokes
                     ])
 
