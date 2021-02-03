b4ad8fd25bdaadbaaff2c587cd671ba3a51ec19d
TanayB11 <tanaybiradar24@gmail.com>
2021-02-03 13:52:44 -0800

    Add app information page

---
+++ client/src/components/Feature.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class='row-span-1' :class='{ "mt-12 md:mt-16": "!isFirstElement"}'>
+			<div class="flex items-center justify-center font-normal">
+				<FeatureIcons :name='name'/>
+				<p class="ml-5">{{ title }}</p>
+			</div>
+			<p class="mt-2 text-left text-center text-md">{{ body }}</p>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import FeatureIcons from '../components/FeatureIcons'
+
+export default defineComponent ({
+	components: {
+		FeatureIcons
+	},
+	props: {
+		title: String,
+		name: String,
+		body: String,
+		isFirstElement: Boolean
+	}
+})
+</script>
+
+<style scoped>
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -45,8 +45,108 @@
         </div>
       </div>
     </div>
-    <div id="about">
-      <!-- TODO: Add about content here -->
+    <!-- TODO: Add about content here -->
+    <div id='about' class='py-4'>
+      <div class="mt-4 md:mt-12">
+        <div class="px-8 text-lg text-center md:text-2xl lg:text-3xl opacity-87">
+          <p>The perfect language learning system didn't exist...</p>
+          <p>until now.</p>
+        </div>
+        <div class='px-3 mt-6 text-sm text-center md:text-lg lg:text-xl opacity-87'>
+          <div>
+            Learn languages with 
+            <a href="https://en.wikipedia.org/wiki/Spaced_repetition" target="_blank" class="font-normal hoverlink">spaced repetition</a>
+            flashcards
+          </div>
+          <p>and practice writing with built-in handwriting recognition.</p>
+        </div>
+        <div class="flex items-center justify-center w-1/2 m-auto lg:mt-10 grid grid-rows-8 grid-cols-1 lg:grid-cols-2 text-md md:text-lg">
+          <div class="col-span-1 lg:pr-8">
+            <Feature name='minimalist' :isFirstElement='true'
+              title='Minimalist Design'
+              body="
+                A modern, simple design with the Nord color palette makes using
+                jadeocr fun and intuitive.
+              "
+            />
+            <Feature name='ocr' :isFirstElement='false'
+              title='Handwriting OCR'
+              body='
+                Computer vision delivers powerful optical character recognition 
+                to help you practice handwriting.
+              '
+            />
+            <Feature name='cloud' :isFirstElement='false'
+              title='Cloud Sync'
+              body='
+                Jadeocr safely stores your decks in the cloud so you 
+                can review no matter where you are.
+              '
+            />
+          </div>
+          <div class="col-span-1 lg:pl-8">
+            <Feature name='reactive' :isFirstElement='true'
+              title='Reactive UI'
+              body="
+                Vue.js powers jadeocr's snappy experience, making 
+                learning new languages a joy.
+              "
+            />
+            <Feature name='spaced-repetition' :isFirstElement='false'
+              title='SRS Cards'
+              body='
+                Spaced repetition allows you to learn more efficiently 
+                and promotes long-term retention.
+              '
+            />
+            <Feature name='open-source' :isFirstElement='false'
+              title='Open Source'
+              body='
+                In the open-source spirit, the
+                code is public on GitHub. Thus,  
+                jadeocr will remain free, forever.
+              '
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mt-24 footer">
+      <hr class="opacity-25">
+      <div id="footer-content" class='py-8 text-xs lg:text-sm'>
+        <div class="text-center grid grid-cols-1 lg:grid-cols-2 opacity-87">
+          <div class="w-1/2 m-auto text-center col-span-1">
+            <p>Tanay Biradar, Jeremy Tow, Riley Kong</p>
+            <p>
+              This project is licensed under the 
+              <a href='https://github.com/TanayB11/jadeocr/blob/master/LICENSE' target="_blank" class='font-normal hoverlink'>
+                MIT License.
+              </a>
+            </p>
+          </div>
+          <!-- Make 2 more cols on small displays -->
+          <div class="w-1/2 m-auto mt-4 font-normal text-center col-span-1 lg:mt-0">
+            <div class='grid grid-cols-2 lg:grid-cols-1'>
+              <div class='col-span-1'>
+                <div>
+                  <a href='https://github.com/TanayB11/jadeocr' target="_blank" class="hoverlink">GitHub</a>
+                </div>
+                <div>
+                  <a href="https://github.com/TanayB11/jadeocr#Donations" target="_blank" class="hoverlink">Donate</a>
+                </div>
+              </div>
+              <div class="col-span-1">
+                <div>
+                  <a href='/terms' target="_blank" class="hoverlink">Terms</a>
+                </div>
+                <div>
+                  <a href='mailto: devs@jadeocr.com' class="hoverlink">Contact</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -54,15 +154,55 @@
 <script lang="ts">
   import { defineComponent } from 'vue'
   import StartLink from '../components/StartLink.vue'
+  import Feature from '../components/Feature.vue'
 
   export default defineComponent({
     name: 'Home',
     components: {
       StartLink,
+      Feature
     },
   })
 </script>
 
 <style scoped>
-  /* TODO: Add scroll cue animation */
+  .hoverlink:hover {
+    opacity: 0.60;
+  }
+
+  .slide {
+    animation: .75s slidedown;
+  }
+
+  #scroll-cue {
+    animation: pulse 3s 5s ease-in-out infinite;
+  }
+
+  @keyframes pulse {
+    50% {
+      transform: scale(.8);
+    }
+
+    100% {
+      transform: none;
+    }
+  }
+
+  @keyframes slidedown{
+    from{
+      margin-top: -5vh;
+      opacity: 0;
+    }
+    to{
+      margin-top: 0vh;
+      opacity: .87;
+    }
+  }
+  /* To deal with weird landscape bug on phones */
+  @media (max-width: 800px) and (orientation:landscape) {
+    #intro {
+      height: 200vh;
+      margin-bottom: 8rem;
+    }
+  }
 </style>
