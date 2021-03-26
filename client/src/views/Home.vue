cbdb28aa892d83daa935c72afcd0c61138f294bb
TanayB11 <tanaybiradar24@gmail.com>
2021-03-25 23:44:18 -0700

    Redesign home page responsive layout

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,185 +1,169 @@
 <template>
   <div id="home">
-    <div id="intro">
-      <div
-        class="mx-auto text-center grid grid-cols-1 lg:grid-cols-2 md:-mt-16 lg:mt-16 xl:-mt-8"
-      >
-        <div class="col-span-1">
-          <img
-            :src="require('@/assets/title-image.png')"
-            alt="An image of the Chinese character 玉"
+    <div class="absolute w-full">
+      <div class="lg:grid lg:grid-cols-2 lg:mt-16 xl:-mt-12 md:-mt-12">
+        <img :src="require('@/assets/title-image.png')" alt="An image of the Chinese character 玉" class="lg:col-span-1">
+        <div class="flex flex-col justify-center -mt-8 text-3xl text-center md:text-5xl lg:col-span-1 lg:text-left md:-mt-32 lg:-mt-8">
+          <p class="leading-normal">Learn languages</p>
+          <p class="leading-normal">without distractions.</p>
+          <start-link linkType="getStartedButton" class="mt-6 md:mt-8" />
+        </div>
+      </div>
+      <div class="mt-10 md:mt-16 xl:-mt-12">
+        <svg
+          class="mx-auto bi bi-chevron-down animate-bounce"
+          width="3em"
+          height="3em"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1.646 4.646a.5.5 0 01.708 0L8 10.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z"
+            clip-rule="evenodd"
           />
-        </div>
-        <div
-          class="flex flex-col justify-center -mt-8 text-center md:-mt-32 col-span-1 lg:mt-0 lg:text-left xl:ml-16"
-        >
-          <div class="text-3xl lg:text-5xl md:text-4xl">
-            <p>Learn languages</p>
-            <p>without distractions.</p>
-          </div>
-          <div>
-            <start-link
-              linkType="getStartedButton"
-              class="mt-6 md:mt-10 lg:mt-6"
-            />
-          </div>
-        </div>
+        </svg>
       </div>
-      <div class="mt-8 md:mt-16 grid grid-rows-1 lg:-mt-10 xl:-mt-20">
-        <div class="m-auto row-span-1">
-          <svg
-            class="bi bi-chevron-down animate-bounce"
-            width="3em"
-            height="3em"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
+    </div>
+    <div>
+      <div id="about" class="py-4">
+        <div class="mt-4 md:mt-12">
+          <div
+            class="px-8 text-lg text-center md:text-2xl lg:text-3xl opacity-87"
           >
-            <path
-              fill-rule="evenodd"
-              d="M1.646 4.646a.5.5 0 01.708 0L8 10.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </div>
-      </div>
-    </div>
-    <div id="about" class="py-4">
-      <div class="mt-4 md:mt-12">
-        <div
-          class="px-8 text-lg text-center md:text-2xl lg:text-3xl opacity-87"
-        >
-          <p>The perfect language learning system didn't exist...</p>
-          <p>until now.</p>
-        </div>
-        <div
-          class="px-3 mt-6 text-sm text-center md:text-lg lg:text-xl opacity-87"
-        >
-          <div>
-            Learn languages with
-            <a
-              href="https://en.wikipedia.org/wiki/Spaced_repetition"
-              target="_blank"
-              class="font-normal hoverlink"
-              >spaced repetition</a
-            >
-            flashcards
+            <p>The perfect language learning system didn't exist...</p>
+            <p>until now.</p>
           </div>
-          <p>and practice writing with built-in handwriting recognition.</p>
-        </div>
-        <div
-          class="flex items-center justify-center w-1/2 m-auto lg:mt-10 grid grid-rows-8 grid-cols-1 lg:grid-cols-2 text-md md:text-lg"
-        >
-          <div class="col-span-1 lg:pr-8">
-            <Feature
-              name="minimalist"
-              :isFirstElement="true"
-              title="Minimalist Design"
-              body="
-                A modern, simple design with the Nord color palette makes using
-                jadeocr fun and intuitive.
-              "
-            />
-            <Feature
-              name="ocr"
-              :isFirstElement="false"
-              title="Handwriting OCR"
-              body="
-                Computer vision delivers powerful optical character recognition 
-                to help you practice handwriting.
-              "
-            />
-            <Feature
-              name="cloud"
-              :isFirstElement="false"
-              title="Cloud Sync"
-              body="
-                Jadeocr safely stores your decks in the cloud so you 
-                can review no matter where you are.
-              "
-            />
-          </div>
-          <div class="col-span-1 lg:pl-8">
-            <Feature
-              name="reactive"
-              :isFirstElement="true"
-              title="Reactive UI"
-              body="
-                Vue.js powers jadeocr's snappy experience, making 
-                learning new languages a joy.
-              "
-            />
-            <Feature
-              name="spaced-repetition"
-              :isFirstElement="false"
-              title="SRS Cards"
-              body="
-                Spaced repetition allows you to learn more efficiently 
-                and promotes long-term retention.
-              "
-            />
-            <Feature
-              name="open-source"
-              :isFirstElement="false"
-              title="Open Source"
-              body="
-                In the open-source spirit, the
-                code is public on GitHub. Thus,  
-                jadeocr will remain free, forever.
-              "
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="mt-24 footer">
-      <hr class="opacity-25" />
-      <div id="footer-content" class="py-8 text-xs lg:text-sm">
-        <div class="text-center grid grid-cols-1 lg:grid-cols-2 opacity-87">
-          <div class="w-1/2 m-auto text-center col-span-1">
-            <p>Tanay Biradar, Jeremy Tow, Riley Kong</p>
-            <p>
-              This project is licensed under the
+          <div
+            class="px-3 mt-6 text-sm text-center md:text-lg lg:text-xl opacity-87"
+          >
+            <div>
+              Learn languages with
               <a
-                href="https://github.com/TanayB11/jadeocr/blob/master/LICENSE"
+                href="https://en.wikipedia.org/wiki/Spaced_repetition"
                 target="_blank"
                 class="font-normal hoverlink"
+                >spaced repetition</a
               >
-                MIT License.
-              </a>
-            </p>
+              flashcards
+            </div>
+            <p>and practice writing with built-in handwriting recognition.</p>
           </div>
-          <!-- Make 2 more cols on small displays -->
           <div
-            class="w-1/2 m-auto mt-4 font-normal text-center col-span-1 lg:mt-0"
+            class="flex items-center justify-center w-1/2 m-auto lg:mt-10 grid grid-rows-8 grid-cols-1 lg:grid-cols-2 text-md md:text-lg"
           >
-            <div class="grid grid-cols-2 lg:grid-cols-1">
-              <div class="col-span-1">
-                <div>
-                  <a
-                    href="https://github.com/jadeocr/jadeocr-next"
-                    target="_blank"
-                    class="hoverlink"
-                    >GitHub</a
-                  >
+            <div class="col-span-1 lg:pr-8">
+              <Feature
+                name="minimalist"
+                :isFirstElement="true"
+                title="Minimalist Design"
+                body="
+                  A modern, simple design with the Nord color palette makes using
+                  jadeocr fun and intuitive.
+                "
+              />
+              <Feature
+                name="ocr"
+                :isFirstElement="false"
+                title="Handwriting OCR"
+                body="
+                  Computer vision delivers powerful optical character recognition 
+                  to help you practice handwriting.
+                "
+              />
+              <Feature
+                name="cloud"
+                :isFirstElement="false"
+                title="Cloud Sync"
+                body="
+                  Jadeocr safely stores your decks in the cloud so you 
+                  can review no matter where you are.
+                "
+              />
+            </div>
+            <div class="col-span-1 lg:pl-8">
+              <Feature
+                name="reactive"
+                :isFirstElement="true"
+                title="Reactive UI"
+                body="
+                  Vue.js powers jadeocr's snappy experience, making 
+                  learning new languages a joy.
+                "
+              />
+              <Feature
+                name="spaced-repetition"
+                :isFirstElement="false"
+                title="SRS Cards"
+                body="
+                  Spaced repetition allows you to learn more efficiently 
+                  and promotes long-term retention.
+                "
+              />
+              <Feature
+                name="open-source"
+                :isFirstElement="false"
+                title="Open Source"
+                body="
+                  In the open-source spirit, the
+                  code is public on GitHub. Thus,  
+                  jadeocr will remain free, forever.
+                "
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mt-24 footer">
+        <hr class="opacity-25" />
+        <div id="footer-content" class="py-8 text-xs lg:text-sm">
+          <div class="text-center grid grid-cols-1 lg:grid-cols-2 opacity-87">
+            <div class="w-1/2 m-auto text-center col-span-1">
+              <p>Tanay Biradar, Jeremy Tow, Riley Kong</p>
+              <p>
+                This project is licensed under the
+                <a
+                  href="https://github.com/TanayB11/jadeocr/blob/master/LICENSE"
+                  target="_blank"
+                  class="font-normal hoverlink"
+                >
+                  MIT License.
+                </a>
+              </p>
+            </div>
+            <!-- Make 2 more cols on small displays -->
+            <div
+              class="w-1/2 m-auto mt-4 font-normal text-center col-span-1 lg:mt-0"
+            >
+              <div class="grid grid-cols-2 lg:grid-cols-1">
+                <div class="col-span-1">
+                  <div>
+                    <a
+                      href="https://github.com/jadeocr/jadeocr-next"
+                      target="_blank"
+                      class="hoverlink"
+                      >GitHub</a
+                    >
+                  </div>
+                  <div>
+                    <a
+                      href="https://github.com/TanayB11/jadeocr#Donations"
+                      target="_blank"
+                      class="hoverlink"
+                      >Donate</a
+                    >
+                  </div>
                 </div>
-                <div>
-                  <a
-                    href="https://github.com/TanayB11/jadeocr#Donations"
-                    target="_blank"
-                    class="hoverlink"
-                    >Donate</a
-                  >
-                </div>
-              </div>
-              <div class="col-span-1">
-                <div>
-                  <a href="/terms" target="_blank" class="hoverlink">Terms</a>
-                </div>
-                <div>
-                  <a href="mailto: devs@jadeocr.com" class="hoverlink"
-                    >Contact</a
-                  >
+                <div class="col-span-1">
+                  <div>
+                    <a href="/terms" target="_blank" class="hoverlink">Terms</a>
+                  </div>
+                  <div>
+                    <a href="mailto: devs@jadeocr.com" class="hoverlink"
+                      >Contact</a
+                    >
+                  </div>
                 </div>
               </div>
             </div>
@@ -191,9 +175,9 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  import StartLink from '../components/StartLink.vue'
-  import Feature from '../components/Feature.vue'
+import StartLink from '@/components/StartLink.vue'
+import Feature from '@/components/Feature.vue'
+import { defineComponent } from 'vue'
 
   export default defineComponent({
     name: 'Home',
@@ -213,6 +197,19 @@
     animation: 0.75s slidedown;
   }
 
+  #scroll-cue {
+    animation: pulse 3s 5s ease-in-out infinite;
+  }
+
+  @keyframes pulse {
+    50% {
+      transform: scale(0.8);
+    }
+
+    100% {
+      transform: none;
+    }
+  }
 
   @keyframes slidedown {
     from {
