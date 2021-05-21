2e35fac4e7d1750a0e8e56cb8887360d5ca31676
wizard1238 <49770335+wizard1238@users.noreply.github.com>
2021-05-20 22:01:16 -0700

    made ocr much more efficient by not loading tensorflow every single time

--- server/src/controllers/ocr.py
+++ server/src/controllers/ocr.py
@@ -1,29 +1,25 @@
 from cnn_chinese_hw.recognizer.TFLiteRecognizer import TFLiteRecognizer
-import sys
-import ast
-
-rec = TFLiteRecognizer()
-# strokes = ast.literal_eval(sys.argv[1])
-
+import json
 from flask import Flask
 from flask import request
+
 app = Flask(__name__)
+
+rec = TFLiteRecognizer()
 
 @app.route('/', methods=['POST'])
 def infer():
-    strokes = request.form['char']
+    strokes = json.loads(request.form['char'])
+    print(strokes)
+    for i in strokes:
+        print(i)
     data = rec.get_L_candidates(strokes,8)
-    return data
-
+    sendData = []
+    for i in data:
+        sendData.append(chr(i[1]))
+    return json.dumps(sendData)
 
 # "[[(75,128),(83,172),(85,196)],[(84,127),(91,124),(123,121),(151,121),(161,121),(161,128),(165,157),(164,181),(162,195)],[(89,187),(103,194),(129,200),(149,199)]]"
-
-# for i in data:
-#     print(i[1])
-
-# print(sys.argv)
-# print(strokes)
-# sys.stdout.flush()
 
 if __name__ == '__main__':
     app.run()
